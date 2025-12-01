1af1e25fdb414a685cf0b057e55aa7c3e16b8060
drios6275 <yosoydrios@gmail.com>
2025-11-30 22:37:40 -0500

    proyectofinal.py

---
+++ proyectofinal.py
@@ -0,0 +1,239 @@
+def funcion_central():
+    print("███████╗██╗███████╗████████╗███████╗███╗   ███╗ █████╗     ██████╗ ███████╗     ██████╗ ███████╗███████╗████████╗██╗ ██████╗ ███╗   ██╗    ")
+    print("██╔════╝██║██╔════╝╚══██╔══╝██╔════╝████╗ ████║██╔══██╗    ██╔══██╗██╔════╝    ██╔════╝ ██╔════╝██╔════╝╚══██╔══╝██║██╔═══██╗████╗  ██║    ")
+    print("███████╗██║███████╗   ██║   █████╗  ██╔████╔██║███████║    ██║  ██║█████╗      ██║  ███╗█████╗  ███████╗   ██║   ██║██║   ██║██╔██╗ ██║ ")   
+    print("╚════██║██║╚════██║   ██║   ██╔══╝  ██║╚██╔╝██║██╔══██║    ██║  ██║██╔══╝      ██║   ██║██╔══╝  ╚════██║   ██║   ██║██║   ██║██║╚██╗██║    ")
+    print("███████║██║███████║   ██║   ███████╗██║ ╚═╝ ██║██║  ██║    ██████╔╝███████╗    ╚██████╔╝███████╗███████║   ██║   ██║╚██████╔╝██║ ╚████║    ")
+    print("╚══════╝╚═╝╚══════╝   ╚═╝   ╚══════╝╚═╝     ╚═╝╚═╝  ╚═╝    ╚═════╝ ╚══════╝     ╚═════╝ ╚══════╝╚══════╝   ╚═╝   ╚═╝ ╚═════╝ ╚═╝  ╚═══╝   ") 
+
+
+    print("" " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " "██████╗ ██╗██████╗ ██╗     ██╗ ██████╗ ████████╗███████╗ ██████╗ █████╗    ") 
+    print("" " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " "██╔══██╗██║██╔══██╗██║     ██║██╔═══██╗╚══██╔══╝██╔════╝██╔════╝██╔══██╗    ")
+    print("" " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " "██████╔╝██║██████╔╝██║     ██║██║   ██║   ██║   █████╗  ██║     ███████║    ")
+    print("" " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " "██╔══██╗██║██╔══██╗██║     ██║██║   ██║   ██║   ██╔══╝  ██║     ██╔══██║    ")
+    print("" " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " "██████╔╝██║██████╔╝███████╗██║╚██████╔╝   ██║   ███████╗╚██████╗██║  ██║    ")
+    print("" " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " "╚═════╝ ╚═╝╚═════╝ ╚══════╝╚═╝ ╚═════╝    ╚═╝   ╚══════╝ ╚═════╝╚═╝  ╚═╝    ")
+
+    print("" " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " "██╗   ██╗██████╗ ███████╗███╗   ███╗")
+    print("" " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " "██║   ██║██╔══██╗██╔════╝████╗ ████║")
+    print("" " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " "██║   ██║██║  ██║█████╗  ██╔████╔██║")
+    print("" " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " "██║   ██║██║  ██║██╔══╝  ██║╚██╔╝██║")
+    print("" " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " "╚██████╔╝██████╔╝███████╗██║ ╚═╝ ██║")
+    print("" " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " ╚═════╝ ╚═════╝ ╚══════╝╚═╝     ╚═╝")
+                                    
+                                                                            
+
+
+funcion_central()
+
+
+def menu_principal():
+    
+    
+    while True:
+        opcion = input("""\nINGRESE LA OPCION: 
+                       R - r) REGISTAR
+                       P - p) PRESTAR 
+                       D - d) RECIBIR DEVOLUCION
+                       C - c) CONSULTAR 
+                       S - s) SALIR \n                                                 : """)
+        if opcion == "R" or opcion == "r":
+            
+            registrar()
+        elif opcion == "P" or opcion == "p":
+            
+            prestar()
+        elif opcion == "D" or opcion == "d":
+            recibir_devoluciones()
+        elif opcion == "C" or opcion == "c":
+            consultar()
+        elif opcion == "S" or opcion == "s":
+            print("salir")
+            break
+        else:
+            print("!ERROR! OPCION INVALIDAD INGRESE UNA OPCION VALIDA PARA CONTINUAR")
+
+
+
+
+libros_registrados = []
+libros_disponibles=[]
+libros_prestados = []
+
+codigo_libros = []
+codigo_libros_disponibles = []
+codigo_libros_prestados = []
+
+
+nombres_estudiantes_prestados = []
+documeno_estudiantes_prestados= []
+
+#----------------------------------------------------------------------REGISTRAR-----------------------------------------------------
+
+def registrar ():
+    print("SELECCIONASTE LA OPCION REGITRAR\n")
+    control_libro = True
+    control_codigo= True
+    nombre_libro_registro = input("ingrese el nombre del libro que quiere registar:  ")
+    
+
+    for i in libros_registrados:
+        if i == nombre_libro_registro:
+            control_libro= False
+    if control_libro == True:
+        libros_registrados.append(nombre_libro_registro)
+        libros_disponibles.append(nombre_libro_registro)
+        codigo = input(f"ingrese el codigo del libro {nombre_libro_registro} :")
+        for k in codigo_libros:
+            if k == codigo:
+                control_codigo = False
+        if control_codigo == True:
+            codigo_libros.append(codigo)
+            codigo_libros_disponibles.append(codigo)
+            
+        else:
+            print("\n!ERROR! EL CODIGO YA ESTA REGISTRADO")
+            libros_registrados.remove(nombre_libro_registro)
+            libros_disponibles.remove(nombre_libro_registro)
+
+
+    else:
+        print("\n!ERROR! EL NOMBRE DEL LIBRO YA ESTA REGISTRADO")
+
+
+
+
+#-------------------------------------------------------------------------PRESTAR--------------------------------------
+def prestar ():
+    
+    
+    control_indice=False
+    print("SELECCIONASTE LA OPCION PRESTAR")
+
+    
+    if (len(libros_registrados)) == 0:
+        print("!ERROR! registra un libro primero")
+    else:
+        
+        for i in range (len(libros_disponibles)):
+            print(f"\nla lista de libros listos  para prestar es:  (# {i}) {libros_disponibles[i]} con codigo: {codigo_libros_disponibles[i]}")
+        seleccion = int(input("\nINGRESE EL (#) DEL LIBRO QUE QUIERE PRESTAR: "))
+        for i in range (len(libros_disponibles)):
+            if seleccion == i:
+                control_indice = True
+        if control_indice== True:
+            print(f"\nINGRESA EL LOS SIGUIENTES DATOS DEL ESTUDIANTE PARA  PRESTAR EL LIBRO: {libros_disponibles[seleccion]} CON CODIGO: {codigo_libros_disponibles[seleccion]}")
+            while True:
+                nombre = input("\nINGRESE EL NOMBRE:")
+                if nombre.isalpha()== False:
+                    print("\n!ERROR! INGRESASTE UN NOMBRE QUE CONTIENE NUMEROS")
+                else:
+                    documento =input(f"\nINGRESE EL DOCUMENTO DEL ESTUDIANTE {nombre} :")
+
+                    if documento.isdigit()== True:
+                        
+                        documeno_estudiantes_prestados.append(documento)
+                        nombres_estudiantes_prestados.append(nombre)
+                        libro = libros_disponibles.pop(seleccion)
+                        codigo = codigo_libros_disponibles.pop(seleccion)
+                        libros_prestados.append(libro)
+                        codigo_libros_prestados.append(codigo)
+
+                        print(f"\nLIBRO PRESTADO CON EXITO  A EL ESTUDIANTE: {nombre} con documento: {documento}")
+                        break
+                    else:
+                        print("\n!ERROR! INCLUISTE LETRAS EN EL DOCUMENTO")
+        else:
+            print("\n!ERRROR! INGRESASTE UN # QUE NO EXISTE EN NUESTRA BASE DE DATOS" )
+
+
+
+#--------------------------------------------------------------------PRESTAR------------------------------------------
+
+
+
+def recibir_devoluciones():
+    control_libros_prestados = False
+    print("SELECCIONASTE LA OPCION DEVOLVER DEVOLUCIONES")
+    if len(libros_prestados)== 0 or len(libros_registrados)==0:
+        print("\n !ERROR!PRESTE UN LIBRO PRIMERO")
+    else:
+        print("\nLos libros prestados son los siguientes\n")
+        for i in range (len(libros_prestados)):
+            print(f" (#{i}) LIBRO: {libros_prestados[i]} Con codigo {codigo_libros_prestados[i]} ")
+        
+        while True:
+            indice = input("\ningrese el # del libro que quiere devolver")
+
+            if indice.isdigit() == False:
+                print("\n!ERROR! INGRESO UNA LETRA O UN DATO NO VALIDO")
+            else:
+                indice= int(indice)
+                break
+        for i in range(len(libros_prestados)):
+            if indice == i:
+                control_libros_prestados = True
+        
+        if control_libros_prestados == False:
+            print("\n!ERROR! EL # DEL LIBRO NO ESTA EN LA BASE DE DATOS")
+        else:
+            
+            print(f"\nLIBRO DEVUELTO CON ÉXITO POR EL ESTUDIANTE: {nombres_estudiantes_prestados[indice]} (Documento: {documeno_estudiantes_prestados[indice]})")
+
+            transeferencia = libros_prestados.pop(indice)
+            libros_disponibles.append(transeferencia)
+            transeferencia_codigo = codigo_libros_prestados.pop(indice)
+            codigo_libros_disponibles.append(transeferencia_codigo)
+            nombre = nombres_estudiantes_prestados[indice]
+            documeto = documeno_estudiantes_prestados[indice]
+            nombres_estudiantes_prestados.remove(nombre)
+            documeno_estudiantes_prestados.remove(documeto)
+
+def consultar ():
+    print("SELECCIONASTE LA OPCION CONSULTAR\n")
+    while True:
+        opcion = input("""ingrese la opcion que quiere consultar \n
+                       L - l) REGISTRADOS 
+                       M - m) DISPONIBLES 
+                       N - n) PRESTADOS
+                       R - r) MENU PRINCIPAL""")
+        
+        if opcion == "L" or opcion == "l":
+            print("QUIERES CONSULTAR LOS LIBROS REGITRADOS : \n")
+
+            if len(libros_registrados) == 0:
+                print("NO TIENES NINGUN LIBRO REGISTRADO")
+            else:
+                print("LIBROS REGISTRADOS:\n")
+                for i in range (len(libros_registrados)):
+                    print(f" EL LIBRO: {libros_registrados[i]} CON CODIGO: {codigo_libros[i]} ESTA EN ESTADO: (REGISTRADO)\n")
+        elif opcion == "M" or opcion == "m":
+            print("QUIERES CONSULTAR LOS LIBROS DISPONIBLES")
+            if len(libros_disponibles) == 0:
+                print("NO TIENES NINGUN LIBRO DISPONIBLE")
+            else:
+                print("LIBROS DISPONIBLES:\n")
+                for i in range (len(libros_disponibles)):
+                    print(f"EL LIBRO: ({libros_disponibles[i]}) CON CODIGO: ({codigo_libros_disponibles[i]}) ESTA EN ESTADO: (DISPONIBLE) \n")
+        elif opcion == "N" or opcion == "n":
+            print("QUIERES CONSULTAR LOS LIBROS PRESTADOS\n")
+            if len(libros_prestados) == 0:
+                print("NO TIENES NINGUN LIBRO PRESTADO")
+            else:
+                print("LIBROS PRESTADOS:")
+                for i in range (len(libros_prestados)):
+                    print(f"EL LIBRO: ({libros_prestados[i]}) CON CODIGO: ({codigo_libros_prestados[i]}) ESTA EN ESTADO : (PRESTADO) POR EL ESTUDIANTE: ({nombres_estudiantes_prestados[i]}) CON N DOCUMENTO ({documeno_estudiantes_prestados[i]})\n")
+        elif opcion == "R" or opcion == "r":
+            break
+        else:
+            print("!ERROR! SELECIONASTE UNA OPCION INVALIDA")
+
+
+menu_principal()      
+
+
+
+
+
+    
+                                                                                                                                           
